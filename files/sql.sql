68ff18519733b4def71fc0ac1672b4a0348a4aeb
Daniil <68991727+fishaffair@users.noreply.github.com>
2023-11-26 12:03:49 +0300

    Update sql.sql

--- files/sql.sql
+++ files/sql.sql
@@ -1,13 +1,10 @@
 create sequence public.cities_id_seq;
-alter sequence cities_id_seq owned by cities.id;
 create sequence public.forecast_id_seq;
-alter sequence forecast_id_seq owned by forecast.id;
-
 create table public.cities (
                                id bigint primary key not null default nextval('cities_id_seq'::regclass),
                                name character varying(255)
 );
-
+alter sequence cities_id_seq owned by cities.id;
 create table public.forecast (
                                  id bigint primary key not null default nextval('forecast_id_seq'::regclass),
                                  "cityId" bigint,
@@ -17,3 +14,4 @@ create table public.forecast (
                                  foreign key ("cityId") references public.cities (id)
                                      match simple on update no action on delete cascade
 );
+alter sequence forecast_id_seq owned by forecast.id;
